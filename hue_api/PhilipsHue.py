4a6658d8fac1ebeb78572b7e6e8f04be715b2a0d
dmitriy <d.arbitman@gmail.com>
2017-05-23 20:41:07 -0700

    updated API

--- hue_api/PhilipsHue.py
+++ hue_api/PhilipsHue.py
@@ -1,5 +1,5 @@
 import sys
-import json
+
 import requests as r
 import time
 
--- hue_api/main.py
+++ hue_api/main.py
@@ -3,22 +3,26 @@ import os
 
 cfg_file = "config.txt"
 
-def main():
-    if os.path.isfile(cfg_file):
-        data = get_bridge_config_data(cfg_file)
-        Hue = PhilipsHue.Bridge(data[0], data[1], True)
-    else:
-        print("config file does not exist\n")
-        Hue = PhilipsHue.Bridge()
-    Hue.set_light("1", True, 254, 1)
+
+def hueinit():
+	global Hue
+	if os.path.isfile(cfg_file):
+		data = get_bridge_config_data(cfg_file)
+		Hue = PhilipsHue.Bridge(data[0], data[1], True)
+	else:
+		print("config file does not exist\n")
+		Hue = PhilipsHue.Bridge()
+
 
 def get_bridge_config_data(filename):
-    try:
-        f = open(filename, "r")
-        data = f.read()
-        data = data.split("\n")
-    finally:
-        f.close()
-    return data
+	try:
+		f = open(filename, "r")
+		data = f.read()
+		data = data.split("\n")
+	finally:
+		f.close()
+	return data
 
-main()
+if __name__ == "__main__":
+	hueinit()
+	Hue.set_light("1", False, 254, 1)
